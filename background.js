1922257339e5a235197b3998226c88563c67726d
John Bieling <john.bieling@gmx.de>
2023-07-20 14:35:56 +0200

    adapt to new locales in XHTML documents

--- background.js
+++ background.js
@@ -26,18 +26,7 @@
 
     messenger.WindowListener.registerChromeUrl([
         ["content",  "mailboxalert",                "content/"],
-        ["resource", "mailboxalert-skin",           "skin/classic/"],
-        ["locale",   "mailboxalert",      "da",     "locale/da/"],
-        ["locale",   "mailboxalert",      "de",     "locale/de/"],
-        ["locale",   "mailboxalert",      "en-US",  "locale/en-US/"],
-        ["locale",   "mailboxalert",      "fr",     "locale/fr/"],
-        ["locale",   "mailboxalert",      "gl-ES",  "locale/gl-ES/"],
-        ["locale",   "mailboxalert",      "it-IT",  "locale/it-IT/"],
-        ["locale",   "mailboxalert",      "nl",     "locale/nl/"],
-        ["locale",   "mailboxalert",      "pl",     "locale/pl/"],
-        ["locale",   "mailboxalert",      "pt-PT",  "locale/pt-PT/"],
-        ["locale",   "mailboxalert",      "sk-SK",  "locale/sk-SK/"],
-        ["locale",   "mailboxalert",      "zh-CN",  "locale/zh-CN/"],
+        ["resource", "mailboxalert-skin",           "skin/classic/"]
     ]);
 
     messenger.WindowListener.registerOptionsPage("chrome://mailboxalert/content/alert_list.xhtml");
